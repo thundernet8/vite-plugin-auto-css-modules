400a0e3a2e6d54159b7da5f5e29a98a94411bd4c
thundernet8 <wuxueqian2010@hotmail.com>
2021-03-01 18:08:39 +0800

    chore(init): init

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # vite-plugin-auto-css-modules
 same as babel-plugin-auto-css-modules but for vite
+
+## install
+
+```bash
+yarn add vite-plugin-auto-css-modules -D
+```
+
+## usage
+
+```ts
+// vite.config.ts
+import { defineConfig } from 'vite';
+import autoCSSModulePlugin from 'vite-plugin-auto-css-modules';
+
+export default defineConfig({
+  // ... other config
+  plugins: [
+    autoCSSModulePlugin(),
+  ],
+});
+```
+
+```tsx
+// this will be modulized css
+import $ from './index.css';
+
+// this will be normal css
+import './index.css';
+
+// you do not need to name your style file to xx.module.css
+```
---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import { Plugin } from "vite";
+import babel, { NodePath } from "@babel/core";
+import { ParserPlugin } from "@babel/parser";
+import { ImportDeclaration } from "@babel/types";
+
+function autoCSSModulePlugin() {
+  return () => {
+    return {
+      visitor: {
+        ImportDeclaration: (path: NodePath<ImportDeclaration>) => {
+          const { node } = path;
+          if (!node) {
+            return;
+          }
+          if (
+            node.specifiers &&
+            node.specifiers.length > 0 &&
+            /\.(css|less|sass|scss|styl|stylus|postcss)($|\?)/.test(
+              node.source.value
+            )
+          ) {
+            const cssFile = node.source.value;
+            node.source.value =
+              cssFile +
+              (cssFile.indexOf("?") > -1 ? "&" : "?") +
+              ".module.styl";
+          }
+        },
+      },
+    };
+  };
+}
+
+function transform(
+  code: string,
+  { sourceMap, file }
+): { code: string; map?: any } {
+  const parsePlugins: ParserPlugin[] = ["jsx"];
+  if (/\.tsx?$/.test(file)) {
+    parsePlugins.push("typescript");
+  }
+  const result = babel.transformSync(code, {
+    configFile: false,
+    parserOpts: {
+      sourceType: "module",
+      allowAwaitOutsideFunction: true,
+      plugins: parsePlugins,
+    },
+    sourceMaps: true,
+    sourceFileName: file,
+    inputSourceMap: sourceMap,
+    plugins: [autoCSSModulePlugin()],
+  });
+
+  return {
+    code: result.code,
+    map: result.map,
+  };
+}
+
+export default function viteAutoCSSModulesPlugin(): Plugin {
+  const name = "vite-plugin-auto-css-modules";
+  return {
+    name,
+    transform(code, id) {
+      if (
+        /\.(js|mjs|ts|jsx|tsx)/.test(id) &&
+        /import\s+([\S]+)\s+from\s+('|")([\S]+)\.(css|less|sass|scss|styl|stylus|postcss)(\?[\S]*)?('|")/.test(
+          code
+        )
+      ) {
+        const result = transform(code, {
+          file: id,
+          sourceMap: this.getCombinedSourcemap(),
+        });
+        if (result) {
+          return result;
+        }
+      }
+      return undefined;
+    },
+  };
+}
